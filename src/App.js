38db9ce09d9968d08e2c372efe0f35b4ac994bce
rendisagala <rendisagala19@gmail.com>
2022-07-31 22:38:39 +0700

    browserrouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import League from "./pages/League";
 import News from "./pages/News";
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <News />
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/league" element={<League />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/League.js
+++ src/pages/League.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
-import Hero from "../components/Hero";
+import Explore from "../components/Explore";
 
-export default function Home() {
+function Home() {
   return (
     <>
       <NavigationBar />
-      <Hero />
+      <Explore />
     </>
   );
 }
+
+export default Home;
